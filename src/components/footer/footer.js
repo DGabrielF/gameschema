30701574a4b3b1205d0d3ab57a489c96562f9e2c
gabriel <dgabrielfof@gmail.com>
2024-05-03 17:34:09 -0300

    :sparkles: feat:adding footer and external links

---
+++ src/components/footer/footer.js
@@ -0,0 +1,16 @@
+export function externalLinks(nameLink) {
+  switch (nameLink) {
+    case "portfolio":
+      window.open("https://dgabrielf.github.io/danilo-ferreira/", "_blank")
+      break;
+    case "github":
+      window.open("http://github.com/DGabrielF", "_blank")
+      break;
+    case "linkedin":
+      window.open("https://www.linkedin.com/in/danilo-gabriel-ferreira-092457127/", "_blank")
+      break;
+    case "itchio":
+      window.open("https://dgabrielf.itch.io/", "_blank")
+      break;
+  }
+}
